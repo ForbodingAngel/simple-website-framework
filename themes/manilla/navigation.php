994f8061d944db5902cea24b91d14faca2b8fab6
ForbodingAngel <forbodingangel@gmail.com>
2024-03-12 23:44:56 -0700

    Break out header text into a class

--- themes/manilla/navigation.php
+++ themes/manilla/navigation.php
@@ -2,41 +2,41 @@
 	<div id="navcontainer">
 		<nav>
 			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="#">WordPress</a>
+				<li><a class="menulabel" href="">Home</a></li>
+                <li><span class="menulabel">WordPress +</span>
 				<!-- First Tier Drop Down -->
 				<ul>
-					<li><a href="#">Themes</a></li>
-					<li><a href="#">Plugins</a></li>
-					<li><a href="#">Tutorials</a></li>
+					<li><a class="menulabel" href="#">Themes</a></li>
+					<li><a class="menulabel" href="#">Plugins</a></li>
+					<li><a class="menulabel" href="#">Tutorials</a></li>
 				</ul>        
 				</li>
-				<li><a href="#">Web Design</a>
+				<li><span class="menulabel">Web Design +</span>
 				<!-- First Tier Drop Down -->
 				<ul>
-					<li><a href="#">Resources</a></li>
-					<li><a href="#">Links</a></li>
-					<li><a href="markdown">Tutorials</a>
+					<li><a class="menulabel" href="#">Resources</a></li>
+					<li><a class="menulabel" href="#">Links</a></li>
+					<li><span class="menulabel">Tutorials +</span>
 				  <!-- Second Tier Drop Down -->
 					<ul>
-						<li><a href="#">HTML/CSS</a></li>
-						<li><a href="#">jQuery</a></li>
-						<li><a href="#">Other</a>
+						<li><a class="menulabel" href="#">HTML/CSS</a></li>
+						<li><a class="menulabel" href="#">jQuery</a></li>
+						<li><span class="menulabel">Other +</span>
 							<!-- Third Tier Drop Down -->
 							<ul>
-								<li><a href="#">Stuff</a></li>
-								<li><a href="#">Things</a></li>
-								<li><a href="#">Other Stuff</a></li>
+								<li><a class="menulabel" href="#">Stuff</a></li>
+								<li><a class="menulabel" href="#">Things</a></li>
+								<li><a class="menulabel" href="#">Other Stuff</a></li>
 							</ul>
 						</li>
 					</ul>
 					</li>
 				</ul>
 				</li>
-				<li><a href="#">Graphic Design</a></li>
-				<li><a href="#">Inspiration</a></li>
-				<li><a href="#">Contact</a></li>
-				<li><a href="#">About</a></li>
+				<li><a class="menulabel" href="#">Graphic Design</a></li>
+				<li><a class="menulabel" href="#">Inspiration</a></li>
+				<li><a class="menulabel" href="#">Contact</a></li>
+				<li><a class="menulabel" href="#">About</a></li>
 			</ul>
 		</nav>
 	</div>
