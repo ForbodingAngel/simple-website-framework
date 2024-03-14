c0bb24ef8a51b82da2cb05eb863530d7b36bb476
ForbodingAngel <forbodingangel@gmail.com>
2024-03-14 12:13:32 -0700

    Add the ability to toggle specific plugins

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-	/*Leaving these for posterity... Parsedown has issues with php8+, so I had to implement a newer parser.*/	
+	/* Leaving these for posterity... Parsedown has issues with php8+, so I had to implement a newer parser. */	
 	//include './required/parsedown/Parsedown.php';
 	//include './required/parsedown/ParsedownExtra.php';
 	
-	/*New Markdown Parser*/
+	/* New Markdown Parser - There is a proper readme in the PHP-Markdown-Parser folder */
 	require './required/PHP-Markdown-Parser/from.php';
 	require './required/PHP-Markdown-Parser/to.php';
 
@@ -15,9 +15,24 @@
     function to_markdown(...$v) {
         return x\markdown\to(...$v);
     }
-	
+
+
+    /* Select a Theme */
 	$theme = "manilla";
+
+    /* Do we want any plugins? */
 	$loadplugins = true;
+
+    if ($loadplugins == true) {
+        /* Choose what plugins you want to load here */
+        
+        /* Before and After Image Slider */
+        $beforeAndAfterSlider = true;
+            
+        /* FontAwesome */
+        $fontAwesome = true;
+    }
+
 
     /* This is a catchall, just in case. There is also another catchall in the page layout file, just in case this one doesn't do the trick. */
     if (!file_exists("pages/" . $pagename .".md")) { 
--- plugins/plugins.php
+++ plugins/plugins.php
@@ -1,9 +1,17 @@
-<?php /* Currently plugins have to be manually wired here. For toggling plugins on and off, look in the theme header */ ?>
+<?php /* Currently plugins have to be manually wired here. For toggling plugins on and off, look in the theme header.
+
+Plugins are only loaded if loadplugins in config.php is set to true */ ?>
+
 
 <?php /* CSS Before and After Slider */ ?>
-<link type="text/css" href="plugins/css-before-and-after-image-slider/css/main.css" rel="stylesheet" />
-<script src="plugins/css-before-and-after-image-slider/js/jquery-3.6.0.min.js"></script>
-<script src="plugins/css-before-and-after-image-slider/js/main.js"></script>
+<?php if ($beforeAndAfterSlider == true) { ?>
+    <link type="text/css" href="plugins/css-before-and-after-image-slider/css/main.css" rel="stylesheet" />
+    <script src="plugins/css-before-and-after-image-slider/js/jquery-3.6.0.min.js"></script>
+    <script src="plugins/css-before-and-after-image-slider/js/main.js"></script>
+<?php } ?>
+
 
 <?php /* FontAwesome 4.7.0 */ ?>
-<link type="text/css" href="plugins/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" />
+<?php if ($fontAwesome == true) { ?>
+    <link type="text/css" href="plugins/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet" />
+<?php } ?>
