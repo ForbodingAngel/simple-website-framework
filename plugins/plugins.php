95493f7ef868f72bfeee0df53c99a951aeba2b28
ForbodingAngel <forbodingangel@gmail.com>
2024-03-14 09:35:43 -0700

    Fix some plugin wiring \n Add responsive Nav to plugins so that I can integrate it \n Remove skeleton css stuff, because it's causing issues

--- plugins/plugins.php
+++ plugins/plugins.php
@@ -1,6 +1,6 @@
 <?php /* Currently plugins have to be manually wired here. For toggling plugins on and off, look in the theme header */ ?>
 
 <?php /* CSS Before and After Slider */ ?>
-<link type="text/css" href="css-before-and-after-image-slider/css/main.css" rel="stylesheet" />
+<link type="text/css" href="plugins/css-before-and-after-image-slider/css/main.css" rel="stylesheet" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="css-before-and-after-image-slider/js/main.js"></script>
+<script src="plugins/css-before-and-after-image-slider/js/main.js"></script>
--- themes/manilla/header.php
+++ themes/manilla/header.php
@@ -32,12 +32,12 @@
 
 	<!-- FONT
 	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-	<? /* Load the Font via CSS */ ?>
+	<?php /* Load the Font via CSS */ ?>
 
 	<!-- CSS
 	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/skeleton.css">
+	<?php /* <link rel="stylesheet" href="css/normalize.css"> */ ?>
+	<?php /* <link rel="stylesheet" href="css/skeleton.css"> */ ?>
 	<link rel="stylesheet" href="css/responsivegridsystem.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="themes/<?php echo $theme; ?>/navigation.css">
